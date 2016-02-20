0b50c851de6e0605da2b350fe3331d24d2c2c6f9
amirbar <amirb4r@gmail.com>
2016-02-20 17:29:47 +0200

    adding go file

--- go.m
+++ go.m
@@ -1,9 +1,9 @@
-function [ ] = go( dirPath, dic )
+function [ ] = go( dirPath)
     addpath('./Utils');
     OUTPUT_FILE_NAME = 'predicted.txt';
     INPUT_VECTORS_FILE_NAME = 'Data/vectors.txt'; %relative to import.m
-    
-    %dic = importfile(INPUT_VECTORS_FILE_NAME);
+    dirPath = strcat(dirPath,'/');
+    dic = importfile(INPUT_VECTORS_FILE_NAME);
     dir_listing = dir(dirPath);
     fileID = fopen(OUTPUT_FILE_NAME,'w');
     formatSpec = '%s %d\n';
@@ -12,13 +12,13 @@ function [ ] = go( dirPath, dic )
     for i=1:size(dir_listing,1)
         if (dir_listing(i).isdir == 0)
             vec = getParVector(strcat(dirPath,dir_listing(i).name), dic);
-            classification_result = svmpredict(ones(size(vec,1)), vec, model);
+            classification_result = svmpredict(ones(size(vec,1)), vec, model,'-q');
             if (classification_result(1,1) == 1)
                 output = 1;
             else 
                 output = 0;
             end
-            fprintf(fileID,formatSpec,dir_listing(i),output);
+            fprintf(fileID,formatSpec,dir_listing(i).name,output);
         end
     end
 end
