22a225db8d1c8a6a0ef59880f16c67a06f9c08e8
amirbar <amirb4r@gmail.com>
2016-02-20 10:31:59 +0200

    added dependencies and generalized training code

--- GenerateTrainingData.m
+++ GenerateTrainingData.m
@@ -1,6 +1,8 @@
-function [] = GenerateTrainingData(dic)
-    %dic = importfile('vectors.txt');
-    fileID = fopen('dataset','w');
+% assuming positive examples are in 'pos/' and negatives in 'neg/'
+
+function [] = GenerateTrainingData(inputVectorsFilePath, outputDatasetFilePath)
+    dic = importfile(inputVectorsFilePath);
+    fileID = fopen(outputDatasetFilePath,'w');
     generateOutputFromDirectory( 'neg/', dic, fileID, -1 )
     generateOutputFromDirectory( 'pos/', dic, fileID, 1 )
 
