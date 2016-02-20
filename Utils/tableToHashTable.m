3f9a6c8b43447512d8eb6b22ef93540b2e9e905e
amirbar <amirb4r@gmail.com>
2016-02-20 17:46:37 +0200

    try catch hashtable

--- Utils/tableToHashTable.m
+++ Utils/tableToHashTable.m
@@ -5,8 +5,12 @@ function [ dic ] = tableToHashTable( rowNames, vectors )
     for i=1:size(rowNames,1)
         word = rowNames(i,1);
         word = word{1};
-        vec = table2array(vectors({word},:));
-        dic(word) = vec;
+ 	try
+            vec = table2array(vectors({word},:));
+            dic(word) = vec;
+        catch ME
+            continue;
+        end
     end
 
 
